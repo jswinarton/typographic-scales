4aa8e8164e68dc5c6c62fbe3defa1446748ffcb6
Jeremy Swinarton <jeremy@swinarton.com>
2018-10-19 16:49:43 -0400

    Add ability to specify intervals and add basic scale library

--- typographic_scales/__init__.py
+++ typographic_scales/__init__.py
@@ -1,31 +1,48 @@
+"""
+For more general info, see http://spencermortensen.com/articles/typographic-scale/
+"""
+import functools
 import math
 
 
 class Scale:
-    def __init__(self, ratio, tones, fundamental_freq):
+    def __init__(self, base, ratio, steps, intervals=None, sig_figs=0):
         self.ratio = ratio
-        self.tones = tones
-        self.fundamental_freq = fundamental_freq
+        self.steps = steps
+        self.base = base
+        self.sig_figs = sig_figs
+
+        if intervals is None:
+            self.intervals = range(steps)
+        else:
+            self.intervals = intervals
 
     def tone(self, tone):
-        return self.fundamental_freq * math.pow(self.ratio, (tone / self.tones))
+        step = self._tone_to_step(tone)
+        tone_value = self.base * math.pow(self.ratio, (step / self.steps))
+        return self._round(tone_value)
 
-    def tone_by_octave(self, octave, tone):
-        if tone > self.tones:
-            raise ScaleError("tone out of range")
-        tone = octave * self.tones + tone
-        return self.tone(tone)
+    def _tone_to_step(self, tone):
+        num_intervals = len(self.intervals)
 
+        octave = math.floor(tone / num_intervals)
+        remainder = tone % num_intervals
 
+        return octave * self.steps + self.intervals[remainder]
+
+    def _round(self, value):
+        return round(value, self.sig_figs)
 
 
 class ScaleError(Exception):
     pass
 
 
-# s = Scale(2, 5, 16)
-# s.tone(0, 0)
-# > 16
-# s.tone(10)
-# > 32
-# s.tone(1, 1)
+
+MajorScale = functools.partial(Scale, ratio=2, steps=12, intervals=[0,4,7])
+
+MinorScale = functools.partial(Scale, ratio=2, steps=12, intervals=[0,3,7])
+
+PentatonicScale = functools.partial(Scale, ratio=2, steps=5)
+
+classical_scale = PentatonicScale(base=12)
