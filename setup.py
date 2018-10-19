3f045eb0de3e498521c22cd01b621bc61f034504
Jeremy Swinarton <jeremy@swinarton.com>
2018-10-18 21:56:59 -0400

    Create python package

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+DESCRIPTION = ''
+
+setup(
+    name='typographic_scales',
+    version='0.1.0',
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
+    author='Jeremy Swinarton',
+    author_email='jeremy@swinarton.com',
+    url='https://github.com/jswinarton/typographic-scales',
+    licence='Public Domain',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+    ],
+)
